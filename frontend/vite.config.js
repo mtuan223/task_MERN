5ab1c64c8eedde6e7eb08241ca3dad36720a6ad2
chocopie223 <phanhoangminhtuan9.12@gmail.com>
2026-01-17 17:48:36 +0700

    adjust CORS and env var of FE

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": "http://localhost:8000",
-    },
+    // proxy: {
+    //   "/api": "http://localhost:8000",
+    // },
   },
   test: {
     environment: "jsdom",
