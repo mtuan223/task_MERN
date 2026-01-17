dfb01b7d7d8b665ed63262ac3bea2df1f3a79ce3
chocopie223 <phanhoangminhtuan9.12@gmail.com>
2026-01-17 17:45:53 +0700

    Cors Added

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./connect/database");
 const port = process.env.PORT || 5000;
+const Cors = require("cors");
 
 connectDB();
 const app = express();
 
 app.use(express.json()); //help parse the json in body to js object
 app.use(express.urlencoded({ extended: false })); //help parse the form in body to js object
+app.use(Cors());
 
 app.use("/api/tasks", require("./routes/taskRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
